55d3b91dbe357b75be32d2479e17b62cb112e2c1
aaglezc <aaglezc@gmail.com>
2020-06-11 21:06:03 -0400

    Initial commit with Menu Skeleton

---
+++ app/src/main/java/com/example/seconlifeps/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.seconlifeps;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+
+import com.example.seconlifeps.Fragments.MainFragment;
+import com.example.seconlifeps.Fragments.ProfileFragment;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
+    NavigationView navigationView;
+
+    // Fragment Manager
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout =  findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigationView);
+
+        //set onclick event to NavigationView
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle( this, drawerLayout, toolbar, R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        actionBarDrawerToggle.syncState();
+
+        //load main fragmennt
+
+       fragmentManager = getSupportFragmentManager();
+       fragmentTransaction = fragmentManager.beginTransaction();
+       fragmentTransaction.add(R.id.container, new ProfileFragment());
+       fragmentTransaction.commit();
+
+
+
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        if (item.getItemId() == R.id.home) {
+
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container, new MainFragment());
+            fragmentTransaction.commit();
+        }
+        if (item.getItemId() == R.id.profile) {
+
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container, new ProfileFragment());
+            fragmentTransaction.commit();
+        }
+
+        return false;
+    }
+}
